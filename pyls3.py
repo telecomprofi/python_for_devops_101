bad83c83130ae29aef798b7cef4ccf6c617ddbd9
Ihor Ko <telecomprofi@gmail.com>
2021-10-15 14:32:26 +0300

    Updated subprocess example with more complex shell example with pipe, | grep

--- pyls3.py
+++ pyls3.py
@@ -1,5 +1,16 @@
-from subprocess import PIPE, run
+#!/usr/bin/env python3
+from subprocess import PIPE, run, Popen
+
+# simple use without pipes, greps etc
 
 command = ['ls', '-l']
 result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
 print(result.returncode, result.stdout, result.stderr)
+
+import subprocess
+
+# more complex with pipes, greps
+cmd = "ps -A|grep --color '[j]ava'"
+ps = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+output = ps.communicate()[0]
+print(output)
